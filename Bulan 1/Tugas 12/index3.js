f51b9fa9870f37d0b0369b7af4d9263571a46a5d
hadirch <hadirich3@gmail.com>
2022-09-07 08:17:25 +0700

    update

--- Bulan 1/Tugas 12/index3.js
+++ Bulan 1/Tugas 12/index3.js
@@ -5,3 +5,21 @@ async function bookFilter() {
 }
 
 // Lanjutkan code untuk menjalankan function filterBookPromise
+async function execute(){
+    filterBooksPromise(true, 50).then((result) => {
+        console.log(result);
+    }).catch((err) => {
+        console.log(err);
+    });
+    try{
+        const result1 = await filterBooksPromise(false, 250)
+        console.log(result1)
+        const result2 = await filterBooksPromise(true, 30)
+        console.log(result2)
+    }
+    catch(err){console.log(err.message)
+    }
+}
+
+execute()
+
