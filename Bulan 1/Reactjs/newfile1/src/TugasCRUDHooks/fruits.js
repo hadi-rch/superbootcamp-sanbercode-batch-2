5dda88c2f50a80dd8377f48daef851619a685523
hadirch <hadirich3@gmail.com>
2022-09-10 10:48:36 +0700

    create

--- Bulan 1/Reactjs/newfile1/src/TugasCRUDHooks/fruits.js
+++ Bulan 1/Reactjs/newfile1/src/TugasCRUDHooks/fruits.js
@@ -11,31 +11,35 @@ const CRUDFruits = ()=>{
     const [inputName, setInputName] = useState("")
     const [inputPrice, setInputPrice] = useState("")
     const [inputWeight, setInputWeight] = useState("")
- 
-
-
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        setDaftarBuah([...daftarBuah,inputName])
-        setDaftarBuah([...daftarBuah,inputPrice])
-        setDaftarBuah([...daftarBuah,inputWeight])
+        setDaftarBuah([...daftarBuah,{nama:inputName, hargaTotal:inputPrice, beratTotal:inputWeight, hargaPerKg:inputPrice/inputWeight*1000}])
+        // setDaftarBuah([...daftarBuah,inputPrice])
+        // setDaftarBuah([...daftarBuah,inputWeight])
         setInputName("")
         setInputPrice("")
         setInputWeight("")
     };
     
-
-
     const handleChange = (event) => {
-        let value = event.target.value.nama
+        let value = event.target.value
+        console.log(value)
         setInputName(value)
-        let value1 = event.target.value.hargaTotal
-        setInputPrice(value1)
-        let value2 = event.target.value.beratTotal
-        setInputWeight(value2)
-
     }
+    const handleChange1 = (event) => {
+        let value1 = event.target.value
+        console.log(value1)
+        setInputPrice(value1)
+    }
+    const handleChange2 = (event) => {
+        let value2 = event.target.value
+        console.log(value2)
+        setInputWeight(value2)
+    }
+
+
+
     return(
             <>
                 <h1>Daftar Harga Buah</h1>
@@ -71,19 +75,19 @@ const CRUDFruits = ()=>{
             <h1>Form Daftar Harga Buah</h1>
             <div className="custom-form">
                     <form onSubmit={handleSubmit}>
-                        <div classname="custom-input">
-                            <label for="fname">Nama :</label>
+                        <div className="custom-input1">
+                            <label htmlFor="name">Nama :</label>
                             <input required autoComplete="off" type="text" name= "name" value={inputName} onChange={handleChange} placeholder="masukan katakata"/>
                         </div>
                         <div className="custom-input2">
-                            <label for="fname">Harga Total :</label>
-                            <input required autoComplete="off" type="text" name="price" value={inputPrice} onChange={handleChange} placeholder="masukan katakata"/>
+                            <label htmlFor="name">Harga Total :</label>
+                            <input required autoComplete="off" type="text" name="price" value={inputPrice} onChange={handleChange1} placeholder="masukan katakata"/>
                         </div>
                         <div className="custom-input3">
-                            <label for="fname">Berat Total(dalam gram) :</label>
-                            <input required autoComplete="off" type="text" name="weight" value={inputWeight} onChange={handleChange} placeholder="masukan katakata"/>
+                            <label htmlFor="name">Berat Total(dalam gram) :</label>
+                            <input required autoComplete="off" type="text" name="weight" value={inputWeight} onChange={handleChange2} placeholder="masukan katakata"/>
                         </div>
-                        <div class="row">
+                        <div className="row">
                                 <input type="submit" value="Submit"/>
                         </div>
                     </form>
